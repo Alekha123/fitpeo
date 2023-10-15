9cb3a0c3a0236fdabb9e7c6b6d1ac0ea11655cfa
Alekha Moharana <92138740+Alekha123@users.noreply.github.com>
2023-10-15 21:57:09 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Body from "./components/Body";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+
+function App() {
+  return (
+    <>
+      {/*<> //React Fragment */}
+      <Sidebar />
+      <Header />
+      <Body />
+    </>
+  );
+}
+
+export default App;
