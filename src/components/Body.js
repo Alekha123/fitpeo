143ffef701c281d5f880f8fc1a672c7a42784090
Alekha Moharana <92138740+Alekha123@users.noreply.github.com>
2023-10-16 22:17:51 +0530

    chart implemeted

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,19 +1,120 @@
-import React from 'react'
+import React from "react";
+import { Bar } from "react-chartjs-2";
+import {
+  BarElement,
+  CategoryScale,
+  Chart as ChartJS,
+  Legend,
+  LinearScale,
+  Title,
+  Tooltip,
+} from "chart.js";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const option = {
+  responsive: true,
+  plugins: {
+    legend: { position: "chartArea" },
+    title: {
+      display: true,
+      text: "Modular Bar Chart",
+    },
+  },
+};
+
+const data = {
+  labels: [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sept",
+    "Oct",
+    "Nov",
+    "Dec",
+  ],
+  datasets: [
+    {
+      label: "Product A",
+      data: [20, 30, 40, 50, 60, 70, 20, 30, 40, 50, 60, 70],
+      backgroundColor: "blue",
+    },
+  ],
+};
 
 const Body = () => {
   return (
-    <div>Body</div>
-  )
-}
+    <>
+      <div className="bg-slate-100">
+        <div className=" flex justify-between mx-auto ">
+          {cardInfo.map((item) => (
+            <div className="w-full m-5 bg-white border-2 " key={item.id}>
+              <p>{item.title}</p>
+              <p>${item.income}</p>
+              <p
+                className={`${
+                  item.monthlyIncome > 10 ? "text-green-800" : "text-red-800"
+                }`}
+              >
+                {item.monthlyIncome}% this month
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+      {/* Chart Bar */}
+      <div className="flex">
+        <div className="w-[80%]">
+          <Bar options={option} data={data} />
+        </div>
+
+        <div className="w-[20%]">{/* <Bar options={option} data={data} /> */}</div>
+      </div>
+    </>
+  );
+};
 
 export default Body;
 
-const data =[
+const cardInfo = [
   {
-    "id" : 1,
-    "title" :"earning",
-    "image" : "",
-    "income" : "$198K",
-    "monthlyIncome" :""
-  }
-]
+    id: 1,
+    title: "Earning",
+    image: "",
+    income: "198K",
+    monthlyIncome: "37.8",
+  },
+  {
+    id: 2,
+    title: "Orders",
+    image: "",
+    income: "2.4K",
+    monthlyIncome: "2",
+  },
+  {
+    id: 3,
+    title: "Balance",
+    image: "",
+    income: "2.4K",
+    monthlyIncome: "2",
+  },
+  {
+    id: 4,
+    title: "Total Sales",
+    image: "",
+    income: "89K",
+    monthlyIncome: "11",
+  },
+];
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,9 +5,9 @@ function Header() {
   return (
     <div className="flex justify-between py-5 bg-slate-100 px-8">
       <div>Hey Shahrukh</div>
-      <div className="">
-      <FaSistrix className="inline-flex mr-2"/> 
-        <input className="border-2" type="text" placeholder="search"/>
+      <div className="relative">
+      <FaSistrix className="inline-flex mr-2 absolute mt-2 ml-2"/> 
+        <input className="border-2 pl-7" type="text" placeholder="search"/>
       </div>
     </div>
   );
